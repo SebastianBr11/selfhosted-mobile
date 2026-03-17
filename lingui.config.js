82eeb2329fc6eac8b44c3b9ea66b7ae94f250bef
Sebastian Breit <sebastian.breitan+github@gmail.com>
2026-03-17 16:58:26 +0100

    build(lingui): setup fallback locale

--- lingui.config.js
+++ lingui.config.js
@@ -3,6 +3,9 @@ import { defineConfig } from '@lingui/cli'
 export default defineConfig({
   sourceLocale: 'en',
   locales: ['de', 'en'],
+  fallbackLocales: {
+    default: 'en',
+  },
   catalogs: [
     {
       path: 'src/locales/{locale}/messages',
