b37c339f0812fe82360de9afc5e391f0223f3ebc
Sebastian Breit <sebastian.breitan+github@gmail.com>
2026-03-17 14:00:37 +0100

    feat(settings): use android native button

--- src/app/settings.tsx
+++ src/app/settings.tsx
@@ -1,23 +1,24 @@
 import { ThemedText } from '@/components/themed-text'
 import { ThemedView } from '@/components/themed-view'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import { TextInput } from '@/components/text-input'
 import { useServicesUrl } from '@/hooks/use-services-url'
-import Button from '@/components/button'
 import { useServices } from '@/hooks/use-services'
+import { TextInput } from '@/components/text-input'
+import { Button, Host, Text } from '@expo/ui/jetpack-compose'
+import { fillMaxWidth, padding } from '@expo/ui/jetpack-compose/modifiers'
 
 export default function SettingsScreen() {
   const { url, setUrl, urlFromEnv, valid, errors } = useServicesUrl()
   const { fetchServices, fetchState } = useServices(url || '')
 
   return (
-    <ThemedView style={{ flex: 1 }} inlineInset type="background">
+    <ThemedView style={{ flex: 1 }} type="background">
       <SafeAreaView style={{ flex: 1 }}>
         <ThemedView style={{ flex: 1, justifyContent: 'center' }}>
           <ThemedText type="title">Settings</ThemedText>
         </ThemedView>
-        <ThemedView style={{ flex: 1, gap: 32 }}>
-          <ThemedView style={{ gap: 8 }}>
+        <ThemedView style={{ flex: 1, gap: 24 }}>
+          <ThemedView inlineInset style={{ gap: 8 }}>
             <ThemedText type="label">
               Services URL{' '}
               {urlFromEnv && (
@@ -34,23 +35,32 @@ export default function SettingsScreen() {
             {!valid && <ThemedText type="error">{errors[0]}</ThemedText>}
           </ThemedView>
           <ThemedView style={{ gap: 8 }}>
-            <Button onPress={fetchServices}>Test connection</Button>
-            {fetchState.didFetch &&
-              (fetchState.fetching ? (
-                <ThemedText type="default">Connecting...</ThemedText>
-              ) : (
-                <>
-                  <ThemedText type={fetchState.success ? 'success' : 'error'}>
-                    {fetchState.success ? 'Connected' : 'Not Connected'}
-                  </ThemedText>
-                  {!fetchState.success &&
-                    fetchState.errors.map((error) => (
-                      <ThemedText type="error" key={error}>
-                        {error}
-                      </ThemedText>
-                    ))}
-                </>
-              ))}
+            <Host matchContents>
+              <Button
+                onPress={fetchServices}
+                modifiers={[fillMaxWidth(), padding(32, 0, 32, 0)]}
+              >
+                <Text>Test connection</Text>
+              </Button>
+            </Host>
+            <ThemedView inlineInset>
+              {fetchState.didFetch &&
+                (fetchState.fetching ? (
+                  <ThemedText type="default">Connecting...</ThemedText>
+                ) : (
+                  <>
+                    <ThemedText type={fetchState.success ? 'success' : 'error'}>
+                      {fetchState.success ? 'Connected' : 'Not Connected'}
+                    </ThemedText>
+                    {!fetchState.success &&
+                      fetchState.errors.map((error) => (
+                        <ThemedText type="error" key={error}>
+                          {error}
+                        </ThemedText>
+                      ))}
+                  </>
+                ))}
+            </ThemedView>
           </ThemedView>
         </ThemedView>
       </SafeAreaView>
