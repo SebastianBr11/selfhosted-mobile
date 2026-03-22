3f49f8ac27120a9c15c52ea9c2cd72ceee7454cf
Sebastian Breit <sebastian.breitan+github@gmail.com>
2026-03-22 16:28:46 +0100

    feat(services-view): use whole height and width

--- src/components/services-view.tsx
+++ src/components/services-view.tsx
@@ -45,74 +45,76 @@ export default function ServicesView() {
 
   return (
     <ThemedView style={{ flex: 1 }} type="background">
-      <SafeAreaView style={{ flex: 1, gap: 20, paddingTop: 40 }}>
-        <FlatList
-          ListHeaderComponent={
-            <ThemedText style={{ paddingVertical: 20 }} type="title">
-              <Trans>Your Services</Trans>
-            </ThemedText>
-          }
-          data={services}
-          numColumns={2}
-          refreshing={fetchState.didFetch && fetchState.fetching}
-          onRefresh={fetchServices}
-          columnWrapperStyle={{ gap: 12 }}
-          style={{ marginInline: InlineInsetSmall }}
-          keyExtractor={(item) => item.id}
-          contentContainerStyle={{
-            gap: 12,
-          }}
-          renderItem={({ item }) => (
-            <Pressable
-              onPress={() => setSelectedServiceId(item.id)}
-              style={{ flex: 1 }}
+      <FlatList
+        ListHeaderComponent={
+          <ThemedText
+            style={{ paddingVertical: 40, marginTop: 20 }}
+            type="title"
+          >
+            <Trans>Your Services</Trans>
+          </ThemedText>
+        }
+        data={services}
+        numColumns={2}
+        refreshing={fetchState.didFetch && fetchState.fetching}
+        onRefresh={fetchServices}
+        columnWrapperStyle={{ gap: 12 }}
+        keyExtractor={(item) => item.id}
+        contentContainerStyle={{
+          gap: 12,
+          marginInline: InlineInsetSmall,
+          paddingVertical: 32,
+        }}
+        renderItem={({ item }) => (
+          <Pressable
+            onPress={() => setSelectedServiceId(item.id)}
+            style={{ flex: 1 }}
+          >
+            <ThemedView
+              type="backgroundElement"
+              style={{
+                borderRadius: 8,
+                padding: 16,
+                paddingInline: 12,
+                gap: 8,
+                alignItems: 'center',
+              }}
             >
               <ThemedView
                 type="backgroundElement"
+                style={{ height: 50, aspectRatio: 1 }}
+              >
+                <Image
+                  contentFit="cover"
+                  source={schemeDependantIcon(scheme, item.iconUrl)}
+                  style={{ flex: 1, width: '100%' }}
+                />
+              </ThemedView>
+              <ThemedView
+                type="backgroundElement"
                 style={{
-                  borderRadius: 8,
-                  padding: 16,
-                  paddingInline: 12,
-                  gap: 8,
+                  flexDirection: 'row',
+                  gap: 6,
                   alignItems: 'center',
+                  justifyContent: 'space-around',
+                  marginInline: 18,
                 }}
               >
-                <ThemedView
-                  type="backgroundElement"
-                  style={{ height: 50, aspectRatio: 1 }}
-                >
-                  <Image
-                    contentFit="cover"
-                    source={schemeDependantIcon(scheme, item.iconUrl)}
-                    style={{ flex: 1, width: '100%' }}
-                  />
-                </ThemedView>
-                <ThemedView
-                  type="backgroundElement"
-                  style={{
-                    flexDirection: 'row',
-                    gap: 6,
-                    alignItems: 'center',
-                    justifyContent: 'space-around',
-                    marginInline: 18,
-                  }}
-                >
-                  <ThemedText style={{ textAlign: 'center' }} type="large">
-                    {item.name}
-                  </ThemedText>
-                </ThemedView>
-                <ThemedText
-                  type="small"
-                  style={{ textAlign: 'center' }}
-                  themeColor="textSecondary"
-                >
-                  {item.description}
+                <ThemedText style={{ textAlign: 'center' }} type="large">
+                  {item.name}
                 </ThemedText>
               </ThemedView>
-            </Pressable>
-          )}
-        />
-      </SafeAreaView>
+              <ThemedText
+                type="small"
+                style={{ textAlign: 'center' }}
+                themeColor="textSecondary"
+              >
+                {item.description}
+              </ThemedText>
+            </ThemedView>
+          </Pressable>
+        )}
+      />
       <Host
         style={{
           position: 'absolute',
