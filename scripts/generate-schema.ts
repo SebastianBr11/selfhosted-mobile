fb38dca4f8c87a742a2b7f8dd890130a6e021b1a
Sebastian Breit <sebastian.breitan+github@gmail.com>
2026-03-23 11:58:36 +0100

    fix(generate-schema): babel macros not usable when running via Bun

--- scripts/generate-schema.ts
+++ scripts/generate-schema.ts
@@ -1,12 +1,17 @@
+import { writeFileSync } from 'fs'
 import { toJsonSchema } from '@valibot/to-json-schema'
 import { serviceSystem } from '@/lib/services.system'
 
-const jsonSchema = toJsonSchema(serviceSystem.schema, {
-  ignoreActions: ['check'],
-})
+export async function generateJsonSchema() {
+  const jsonSchema = toJsonSchema(serviceSystem.schema, {
+    ignoreActions: ['check'],
+  })
 
-jsonSchema.$id =
-  'https://github.com/SebastianBr11/selfhosted-mobile/raw/refs/heads/main/schema.json'
+  jsonSchema.$id =
+    'https://github.com/SebastianBr11/selfhosted-mobile/raw/refs/heads/main/schema.json'
 
-const jsonString = JSON.stringify(jsonSchema, null, 2)
-await Bun.write('schema.json', jsonString)
+  const jsonString = JSON.stringify(jsonSchema, null, 2)
+  writeFileSync('schema.json', jsonString)
+}
+
+generateJsonSchema()
