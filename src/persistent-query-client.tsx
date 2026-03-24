9d885f4ab6a40f59f286a04b6d6bb35ec2b9c7c6
Sebastian Breit <sebastian.breitan+github@gmail.com>
2026-03-24 14:49:28 +0100

    chore(persistent-query-client): setup react query dev tools

--- src/persistent-query-client.tsx
+++ src/persistent-query-client.tsx
@@ -1,3 +1,4 @@
+import { useReactQueryDevTools } from '@dev-plugins/react-query'
 import { createAsyncStoragePersister } from '@tanstack/query-async-storage-persister'
 import { QueryClient } from '@tanstack/react-query'
 import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client'
@@ -35,6 +36,7 @@ export function PersistentQueryClientProvider({
 }: {
   children: React.ReactNode
 }) {
+  useReactQueryDevTools(queryClient)
   return (
     <PersistQueryClientProvider
       client={queryClient}
