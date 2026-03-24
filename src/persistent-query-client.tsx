3e01644f1501487f412f16bd72ceb4c9f8842852
Sebastian Breit <sebastian.breitan+github@gmail.com>
2026-03-24 13:01:42 +0100

    chore(react-query): setup persist query client provider

---
+++ src/persistent-query-client.tsx
@@ -0,0 +1,40 @@
+import { createAsyncStoragePersister } from '@tanstack/query-async-storage-persister'
+import { QueryClient } from '@tanstack/react-query'
+import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client'
+import { createMMKV } from 'react-native-mmkv'
+
+const storage = createMMKV()
+
+const clientStorage = {
+  getItem: (key: string) => {
+    const value = storage.getString(key)
+    return value === undefined ? null : value
+  },
+  removeItem: (key: string) => {
+    storage.remove(key)
+  },
+  setItem: (key: string, value: string) => {
+    storage.set(key, value)
+  },
+}
+
+const clientPersister = createAsyncStoragePersister({
+  storage: clientStorage,
+})
+
+const queryClient = new QueryClient()
+
+export function PersistentQueryClientProvider({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <PersistQueryClientProvider
+      client={queryClient}
+      persistOptions={{ persister: clientPersister }}
+    >
+      {children}
+    </PersistQueryClientProvider>
+  )
+}
