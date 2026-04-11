160d50716227f1f5c99365379a6fca49af49d3ba
Sebastian Breit <sebastian.breitan+github@gmail.com>
2026-04-11 17:22:09 +0200

    refactor(data-loaders): make dataloader no longer union with undefined

--- src/features/services/lib/data-loaders/types.ts
+++ src/features/services/lib/data-loaders/types.ts
@@ -9,33 +9,31 @@ export type DataLoader<
   Credentials = unknown,
 > = Partial<Record<Id, LoaderEntry<PublicData, SecretData, Credentials>>>
 
-export type LoaderEntry<PublicData, SecretData, Credentials> =
-  | undefined
-  | {
-      /**
-       * Set this, if there is a preferred way to check for updates instead of using the VCS.
-       * Can be useful, if the service has endpoints for checking for updates.
-       */
-      checkForUpdates?: (
-        serviceUrl: ServiceUrl,
-        version: SemanticVersion,
-      ) => Promise<UpdateData | { hasUpdate: false }>
-      /**
-       * Returns true if the service is healthy
-       */
-      checkHealth: (serviceUrl: ServiceUrl) => Promise<boolean>
-      loadPublicData: (serviceUrl: ServiceUrl) => Promise<{
-        data: PublicData
-        version: SemanticVersion
-      }>
-      loadSecretData?: (credentials: Credentials) => Promise<SecretData>
-      /** Used for checking for updates */
-      repo?: {
-        name: string
-        /** Version Control System */
-        vcs: 'codeberg' | 'github'
-      }
-    }
+export type LoaderEntry<PublicData, SecretData, Credentials> = {
+  /**
+   * Set this, if there is a preferred way to check for updates instead of using the VCS.
+   * Can be useful, if the service has endpoints for checking for updates.
+   */
+  checkForUpdates?: (
+    serviceUrl: ServiceUrl,
+    version: SemanticVersion,
+  ) => Promise<UpdateData | { hasUpdate: false }>
+  /**
+   * Returns true if the service is healthy
+   */
+  checkHealth: (serviceUrl: ServiceUrl) => Promise<boolean>
+  loadPublicData: (serviceUrl: ServiceUrl) => Promise<{
+    data: PublicData
+    version: SemanticVersion
+  }>
+  loadSecretData?: (credentials: Credentials) => Promise<SecretData>
+  /** Used for checking for updates */
+  repo?: {
+    name: string
+    /** Version Control System */
+    vcs: 'codeberg' | 'github'
+  }
+}
 
 export type UpdateData = {
   changelog?: string
