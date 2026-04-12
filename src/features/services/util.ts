c7a712eef5c170e02b057dd768c52521373825ba
Sebastian Breit <sebastian.breitan+github@gmail.com>
2026-04-12 13:47:25 +0200

    chore(services): remove console logs

--- src/features/services/util.ts
+++ src/features/services/util.ts
@@ -10,17 +10,13 @@ export function compareSemanticVersions(
   a: SemanticVersion,
   b: SemanticVersion,
 ) {
-  console.log(a.raw, b.raw)
   if (a.major !== b.major) {
-    console.log('major bigger', a.major > b.major ? a.raw : b.raw)
     return a.major > b.major ? 1 : -1
   }
   if (a.minor !== b.minor) {
-    console.log('minor bigger', a.minor > b.minor ? a.raw : b.raw)
     return a.minor > b.minor ? 1 : -1
   }
   if (a.patch !== b.patch) {
-    console.log('patch bigger', a.patch > b.patch ? a.raw : b.raw)
     return a.patch > b.patch ? 1 : -1
   }
   return 0
