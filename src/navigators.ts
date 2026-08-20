16f572773c693d6153ad271ec43e6ddc1db2ee9e
Sebastian Breit <41546861+SebastianBr11@users.noreply.github.com>
2026-08-20 17:45:24 +0200

    build(deps): Remove react-navigation
    `expo-router` no longer depends on `react-navigation`.
    All imports of `react-navigation` were changed using the codemod 
    `sdk-56-expo-router-react-navigation-replace`

--- src/navigators.ts
+++ src/navigators.ts
@@ -2,8 +2,8 @@ import {
   createMaterialTopTabNavigator,
   MaterialTopTabNavigationEventMap,
   MaterialTopTabNavigationOptions,
-} from '@react-navigation/material-top-tabs'
-import { ParamListBase, TabNavigationState } from '@react-navigation/native'
+} from "expo-router/js-top-tabs"
+import { ParamListBase, TabNavigationState } from "expo-router/react-navigation"
 import { withLayoutContext } from 'expo-router'
 
 const OriginalMaterialTopTabs = createMaterialTopTabNavigator()
