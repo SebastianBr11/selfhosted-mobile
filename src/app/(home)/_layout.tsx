f35c90397ff9fc7345866a7eba6789fbd3daf9cc
Sebastian Breit <sebastian.breitan+github@gmail.com>
2026-08-26 11:23:53 +0200

    Use more expo UI (#57)
    * refactor(remote-source-settings-view): use Expo UI
    
    * refactor(services-view): use Expo UI for the placeholder
    
    * refactor(local-source-settings-view): set inline padding on LazyColumn
    
    * chore(i18n): generate translations

--- src/app/(home)/_layout.tsx
+++ src/app/(home)/_layout.tsx
@@ -1,9 +1,20 @@
 import { Stack } from 'expo-router'
+import { useTheme } from '@/hooks/use-theme'
 
 export default function HomeLayout() {
+  const theme = useTheme()
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="index" />
+    <Stack
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <Stack.Screen
+        name="index"
+        options={{
+          contentStyle: { backgroundColor: theme.background },
+        }}
+      />
       <Stack.Screen
         name="services/[serviceId]"
         options={{ presentation: 'transparentModal' }}
